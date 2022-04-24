74a760f05982d38bd6de12920355252b383c1bbe
Andrew Aslakson <andrew01177@gmail.com>
2022-04-24 15:30:24 -0400

    finished main components

--- src/main/java/my/plug/plugWork/manager/PlugManager.java
+++ src/main/java/my/plug/plugWork/manager/PlugManager.java
@@ -3,15 +3,28 @@ package my.plug.plugWork.manager;
 import my.plug.plugWork.annotation.Plug;
 import my.plug.plugWork.annotation.Power;
 import my.plug.plugWork.annotation.Socket;
+import my.plug.plugWork.annotation.Source;
+import my.plug.plugWork.annotation.Start;
 import my.plug.plugWork.annotation.WireStation;
 
 import my.plug.plugWork.container.WiringStation;
+import my.plug.plugWork.exception.ImpossibleDependencyException;
 import my.plug.plugWork.exception.InstantiationException;
 import my.plug.plugWork.exception.PlugWorkConfigurationException;
+
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 
-import java.util.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class PlugManager {
 
@@ -19,7 +32,7 @@ public class PlugManager {
     private static Map<String, Object> plugs= new HashMap<>();
 
     private static Set<Class> classSet = new HashSet<>();
-    private static Map<Class, Set<Class>> dependencyMap = new HashMap<>();
+    private static Map<String, Map<String, Field>> dependencyMap = new HashMap<>();
 
     public static Map<String, Object> powers = new HashMap<>();
 
@@ -37,7 +50,7 @@ public class PlugManager {
 
         createPowerPlugs();
 
-        injectPlugsInSources();
+        turnOn();
     }
 
     private static void setClassSet(String prefix) {
@@ -59,15 +72,18 @@ public class PlugManager {
 
     private static void buildDependencyMap() {
         classSet.stream().filter(clazz -> clazz.isAnnotationPresent(Power.class)).forEach(clazz -> {
-            Set<Class> dependencies = new HashSet<>();
-            Arrays.stream(clazz.getDeclaredFields()).forEach(field -> {
-                if (field.isAnnotationPresent(Socket.class)) {
-                    dependencies.add(field.getType());
+            Map<String, Field> dependencies = new HashMap<>();
+            String powerName = ((Power)clazz.getAnnotation(Power.class)).name();
+            if (powerName.equals("")) powerName = clazz.getName();
+            Arrays.stream(clazz.getDeclaredFields()).forEach(socket -> {
+                if (socket.isAnnotationPresent(Socket.class)) {
+                    String socketName = socket.getAnnotation(Socket.class).name();
+                    if (socketName.equals("")) socketName = socket.getType().getName();
+                    dependencies.put(socketName, socket);
                 }
             });
-            dependencyMap.put(clazz, dependencies);
+            dependencyMap.put(powerName, dependencies);
         });
-        String yum = "yum";
     }
 
     private static void createWiringStationPlugs() {
@@ -75,17 +91,93 @@ public class PlugManager {
             Arrays.asList(station.getClazz().getDeclaredMethods()).stream()
                     .filter(method -> method.isAnnotationPresent(Plug.class))
                     .forEach(plugMethod -> {
-                        String name = plugMethod.getAnnotation(Plug.class).name();
-                        if (name.equals("")) name = plugMethod.getReturnType().getName();
-                        Object plug = plugMethod.invoke(station.getStation());
-                        plugs.put(name, plug);
+                        try {
+                            String name = plugMethod.getAnnotation(Plug.class).name();
+                            if (name.equals("")) name = plugMethod.getReturnType().getName();
+                            Object plug = plugMethod.invoke(station.getStation());
+                            plugs.put(name, plug);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            throw new InstantiationException("Failed to instantiate level 1 plugs.");
+                        }
                     });
         });
     }
 
     private static void createPowerPlugs() {
+        classSet.stream().filter(clazz -> clazz.isAnnotationPresent(Power.class)).forEach(clazz -> {
+            try {
+                String name = ((Power)clazz.getAnnotation(Power.class)).name();
+                if (name.equals("")) name = clazz.getName();
+                Object power = clazz.getDeclaredConstructor().newInstance();
+                powers.put(name, power);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new InstantiationException("Failed to instantiate Power Plugs.");
+            }
+        });
+
+        do {
+            int previousSize = dependencyMap.size();
+
+            powers.forEach((name, power) -> {
+                Map<String, Field> dependencies = dependencyMap.get(name);
+
+                dependencies.forEach((socketName, socket) -> {
+                    try {
+                        if (plugs.containsKey(socketName)) {
+                            socket.set(power, plugs.get(socketName));
+                            dependencies.remove(socketName);
+                        } else if (powers.containsKey(socketName)) {
+                            socket .set(power, powers.get(socketName));
+                            dependencies.remove(socketName);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        throw new InstantiationException("Failed to inject Socket into Power");
+                    }
+                });
+
+                if (dependencies.isEmpty()) dependencyMap.remove(name);
+            });
+
+            if (previousSize == dependencyMap.size()) throw new ImpossibleDependencyException("Detected Impossible Dependencies, failed to fill all sockets");
+        } while (dependencyMap.size() > 0);
     }
 
-    private static void injectPlugsInSources(){
+    private static void turnOn() {
+        List<Class> sourceClasses = classSet.stream().filter(clazz -> clazz.isAnnotationPresent(Source.class)).collect(Collectors.toList());
+        if (sourceClasses.size() == 0 || sourceClasses.size() > 1)
+            throw new PlugWorkConfigurationException("1 and only 1 class may be annotated with source, found: " + sourceClasses.size());
+
+        Object source = powers.get(((Power)sourceClasses.get(0).getAnnotation(Power.class)).name());
+
+        List<Method> startMethods = Arrays.stream(sourceClasses.get(0).getDeclaredMethods()).filter(method -> method.isAnnotationPresent(Start.class)).collect(Collectors.toList());
+        if (startMethods.size() == 0 || startMethods.size() > 1)
+            throw new PlugWorkConfigurationException("1 and only 1 method may be marked as start method in source class, found: " + startMethods.size());
+
+        Method startMethod = startMethods.get(0);
+
+        sourceClasses = null;
+        startMethods = null;
+
+        cleanUp();
+
+        try {
+            startMethod.invoke(source);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new PlugWorkConfigurationException("Failed to start application");
+        }
+    }
+
+    private static void cleanUp() {
+        wiringStations = null;
+        plugs= null;
+
+        classSet = null;
+        dependencyMap = null;
+
+        powers = null;
     }
 }
